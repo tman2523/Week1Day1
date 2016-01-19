b136ba1e9ab47271ce15fdfe93c60038000db1d5
Tommy Gaessler <tommygaessler@Tommys-MacBook-Pro.local>
2016-01-19 16:40:31 -0700

    Finished HW.

--- main.js
+++ main.js
@@ -4,22 +4,92 @@
 // =======================================================
 
 //1. Log your name to the console (as a string).
+
 console.log("Tommy")
+
 //2. Declare a variable called name, log it to the console.
+
+var name = "Tommy"
+
+console.log(name)
 
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
 
+var number1 = 5
+
+var number2 = 10
+
+number1 + number2
+15
+number2 - number1
+5
+number1 * number2
+50
+number2 / number1
+2
+
 //4. Declare two variables with string values. Create a third variable that concatenates them.
+
+var greeting = "Hello"
+
+var to = " Jenny"
+
+var concatenate = greet
+
+var concatenate = greeting + to
+
+console.log(concatenate)
+
+Hello Jenny
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 
+"5" + "3"
+"53"
+
+//Adding the strings 5 and 3 concatenates them. Using the opperator + on strings concatantes them, any other opperator does the math.
+
+"5" - "3"
+2
+"5" * "3"
+15
+"5" / "3"
+1.6666666666666667
+
 //6. Create a prompt that asks a user to pick a number. Double the answer.
+
+var number = prompt("Pick a number!")
+
+console.log(number * 2)
 
 //7. Create a custom alert.
 
+alert("Custom Alert")
+
 //8. Declare three variables called length, width and height. Calculate area and volume.
+
+var length = 5
+
+var width = 2
+
+var height = 10
+
+console.log(height * width * length)
+100
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
 
+var name = prompt("What is your name?")
+
+var age = prompt("What is your age?")
+
+var city = prompt("What city do you live in?")
+
+confirm("My name is " + name + ", I'm " + age + " years old, and I live in " + city + "!")
+
 //10. Create an alert that uses the response from a prompt.
+
+var response = prompt("What is your favorite color?")
+
+alert(response)
